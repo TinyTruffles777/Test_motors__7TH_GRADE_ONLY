7e8320960370c828fe360b52c791839a1b4450bd
Truffles <BrokeDontBullyMe@outlook.com>
2026-04-21 14:14:59 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 input.onGesture(Gesture.EightG, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Skull)
 })
 input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
     wuKong.setAllMotor(60, -50)
     basic.pause(500)
     wuKong.stopAllMotor()
@@ -15,29 +16,29 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onGesture(Gesture.FreeFall, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Ghost)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.SmallSquare)
 })
 input.onGesture(Gesture.SixG, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Giraffe)
+    basic.showIcon(IconNames.Rabbit)
 })
 input.onGesture(Gesture.ScreenUp, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Triangle)
 })
 input.onGesture(Gesture.ScreenDown, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Sword)
 })
 input.onGesture(Gesture.TiltRight, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Square)
 })
 input.onGesture(Gesture.ThreeG, function () {
     wuKong.stopMotor(wuKong.MotorList.M1)
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Chessboard)
 })
